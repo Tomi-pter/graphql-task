2e000795ca8348f2c724201b665e1f2538507a42
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-08-07 12:24:14 +0100

    task solution

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { v4 as uuidv4 } from "uuid";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +14,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {data.data.jobs.map((job) => (
+          <div className={styles.row} key={uuidv4()}>
+            <div className={styles.company}>
+              <h1>{job.title}</h1>
+              <p>{job.company.name}</p>
+            </div>
+            <div className={styles.tags}>
+              {job.tags.slice(0, 3).map((tag) => (
+                <span key={uuidv4()}>{tag.name}</span>
+              ))}
+            </div>
+            <div className={styles.location}>
+              {job.locationNames ? (
+                <p>{job.locationNames}</p>
+              ) : job.cities.length > 0 ? (
+                job.cities.map((city, index) => (
+                  <span key={uuidv4()}>{(index ? ", " : "") + city.name}</span>
+                ))
+              ) : (
+                <p>Remote</p>
+              )}
+            </div>
+          </div>
+        ))}
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +46,51 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://api.graphql.jobs", {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `{
+        jobs{
+            title
+            company{
+              name
+            }
+            tags{
+              name
+          }
+            cities{
+              name
+            }
+            locationNames
+          }
+        companies{
+            name
+            logoUrl
+            }
+      }`,
+    }),
+  });
+
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
